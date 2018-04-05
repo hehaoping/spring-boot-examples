d83287ff548ed52bef04906ade2fcc4a35dfbf58
hehaoping <1228929031@qq.com>
2018-04-05 21:41:47 +0800

    Add files via upload
    提交初始化文件

---
+++ spring-boot-basic/src/main/java/com/naruto/controller/SampleController.java
@@ -0,0 +1,19 @@
+package com.naruto.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author hhp
+ * @mail 1228929031@qq.com
+ * @date 2018年4月5日
+ */
+@RestController
+public class SampleController {
+
+	@RequestMapping("/hello")
+	public String hello(){
+		return "hello spring boot";
+	}
+	
+}
