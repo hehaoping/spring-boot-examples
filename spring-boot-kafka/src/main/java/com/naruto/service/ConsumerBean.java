6e02b2bb59318a9350c0233d22b55d7a60aa1bb6
hehaoping <1228929031@qq.com>
2018-04-21 23:23:13 +0800

    Add files via upload

---
+++ spring-boot-kafka/src/main/java/com/naruto/service/ConsumerBean.java
@@ -0,0 +1,53 @@
+package com.naruto.service;
+
+import java.util.List;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.Acknowledgment;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author hhp
+ * @mail 1228929031@qq.com
+ * @date 2018年4月21日
+ */
+@Component
+public class ConsumerBean {
+
+	@Value("${my.kafka.topic}")
+	private String kafkaTopic;
+
+	//简单单一消费
+	// @KafkaListener(topics="${my.kafka.topic}")
+	// public void receive(ConsumerRecord<?, ?> cr){
+	// String topic = cr.topic();
+	// String key = cr.key().toString();
+	// String value = cr.value().toString();
+	// System.out.println("消费了消息：");
+	// System.out.println("topic:"+topic+" key:"+key+" value:"+value);
+	// }
+
+	
+	//批量消费
+	@KafkaListener(topics = "${my.kafka.topic}", containerFactory = "batchContainerFactory")
+	public void listen(List<ConsumerRecord<?, ?>> crList,Acknowledgment ack) {
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		System.out.println("records.size: " + crList.size() + " in all");
+		for (ConsumerRecord<?, ?> cr : crList) {
+			String topic = cr.topic();
+			String key = cr.key().toString();
+			String value = cr.value().toString();
+			System.out.println("topic:" + topic + " key:" + key + " value:" + value);
+		}
+		 System.out.println("start commit offset");
+		 ack.acknowledge();//手动提交偏移量
+		 System.out.println("stop commit offset");
+	}
+
+}
